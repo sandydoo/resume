dc884a7b7d6d5ef3dd647a9e6692c9eee7a3cc30
sandydoo <hey@sandydoo.me>
2022-03-30 19:16:43 +0400

    Add tex packages and fonts to nix shell

--- flake.nix
+++ flake.nix
@@ -3,17 +3,23 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.tex2nix-flake.url = "github:Mic92/tex2nix";
+  inputs.tex2nix-flake.inputs.utils.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, tex2nix-flake }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+
         ghc = pkgs.ghc;
+
+        tex-env = import ./nix/tex-env.nix { texlive = pkgs.texlive; };
+        tex2nix = import tex2nix-flake { inherit pkgs; };
       in
       rec {
-        defaultPackage = ghc.callPackage ./default.nix { };
+        defaultPackage = ghc.callPackage ./nix/resume.nix { };
 
         devShell = pkgs.mkShell {
           packages = with pkgs; [
@@ -23,6 +29,10 @@
             haskell-language-server
             stylish-haskell
             zlib.dev
+            tex2nix
+            tex-env
+            inter
+            ibm-plex
           ];
 
           # inputsFrom = [ defaultPackage ];
